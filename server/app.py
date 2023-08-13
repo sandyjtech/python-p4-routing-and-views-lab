8af1fbdf4676905c14b58d11ab5026fbad55c13f
sandyjtech <sandyjtech@gmail.com>
2023-08-13 10:56:37 -0700

    added flask routing

--- server/app.py
+++ server/app.py
@@ -1,8 +1,44 @@
-#!/usr/bin/env python3
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    count = '\n'.join(str(i) for i in range(0, param + 1))
+    return count
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+
+    return str(result)
+
+
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
+
+
